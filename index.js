68193151c330823dac816558da5da65c509e62fa
alemb210 <99274936+alemb210@users.noreply.github.com>
2022-06-02 11:03:08 -0400

    Add files via upload
    added checkWin function to implement scalable win conditions

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 var gameGrid = []
 var idNum
 var size
+var sizeSq
 var turn = 0
 var elementNum = 0
 var newElementId
-var blank = "?"
+var blank = "⠀"
+var flexId
 //const winConditions = [[0, 1, 2], [0, 3, 6], [0, 4, 8], [1, 4, 7], [2, 5, 8], [2, 4, 6], [3, 4, 5], [6, 7, 8]]
 
 
 window.onload = (event) =>
 {
     size = prompt("Enter a size")
+    sizeSq = size * size
     alert("The size is " + size)
     let sizeStr = size * 120 + "px"
     document.getElementById("flex-container").style.width = sizeStr
@@ -39,8 +42,7 @@ function addElement() {
     console.log(newBox)
   }
 
-function tic(callBox)
-{
+function tic(callBox){
     if(callBox.innerText != blank)
     {
         alert("This space is occupied!")
@@ -56,18 +58,81 @@ function tic(callBox)
         turn = 0
     }
     idNum = callBox.getAttribute("id").substring(4)
-    gameGrid[idNum - 1] = callBox.innerText
-    for(const condition of winConditions)
-    {
-        if(callBox.innerText == gameGrid[condition[0]] && callBox.innerText == gameGrid[condition[1]] && callBox.innerText == gameGrid[condition[2]])
-        {
-            alert("Player "+ callBox.innerText + " wins!!")
-        }
-    }
-    console.log(gameGrid)
+    checkWin(callBox.innerText)
+    // gameGrid[idNum - 1] = callBox.innerText
+    // for(const condition of winConditions)
+    // {
+    //     if(callBox.innerText == gameGrid[condition[0]] && callBox.innerText == gameGrid[condition[1]] && callBox.innerText == gameGrid[condition[2]])
+    //     {
+    //         alert("Player "+ callBox.innerText + " wins!!")
+    //     }
+    // }
+    // console.log(gameGrid)
 }
 
-function checkWin()
+function checkWin(sign)
 {
-    
+    let bool = true
+    let sizeInt = parseInt(size)
+    console.log("Sign: " + sign + "\nBool = " + bool + "\nSizeSq: " + sizeSq)
+    for(let x = 0; x <= ((sizeInt - 1) * sizeInt); x = x + sizeInt)
+    {
+        for(let y = x; y <= x + (sizeInt - 1); y++)
+        {
+            flexId = "flex" + y
+            console.log("FlexID: " + flexId + "\nX: " + x + "\nY: " + y)
+            if(document.getElementById(flexId).innerText != sign)
+            {
+                bool = false;
+            }
+        }
+        if(bool)
+        {
+            alert("Player " + sign + " wins!!!")
+        }
+    }
+    bool = true
+    for(x = 0; x <= (sizeInt - 1); x++)
+    {
+        for(y = x; y < x + ((sizeInt - 1) * sizeInt); y = y + sizeInt)
+        {
+            flexId = "flex" + y
+            console.log("FlexID: " + flexId + "\nX: " + x + "\nY: " + y)
+            if(document.getElementById(flexId).innerText != sign)
+            {
+                bool = false;
+            }
+        }
+        if(bool)
+        {
+            alert("Player " + sign + " wins!!!")
+        }
+    }
+    bool = true
+    for(x = 0; x < sizeSq; x = x + (sizeInt + 1))
+    {
+        flexId = "flex" + x
+        console.log("FlexID: " + flexId + "\nX: " + x + "\nY: " + y)
+        if(document.getElementById(flexId).innerText != sign)
+        {
+            bool = false;
+        }
+    }
+    if(bool)
+    {
+        alert("Player " + sign + "win!!!")
+    }
+    for(x = (sizeInt - 1); x <= sizeInt * (sizeInt - 1); x = x + (sizeInt - 1))
+    {
+        flexId = "flex" + x
+        console.log("FlexID: " + flexId + "\nX: " + x + "\nY: " + y)
+        if(document.getElementById(flexId).innerText != sign)
+        {
+            bool = false;
+        }
+    }
+    if(bool)
+    {
+        alert("Player " + sign + " wins!!!")
+    }
 }
